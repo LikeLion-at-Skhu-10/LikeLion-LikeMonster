b6e38bd2868e28e7f8992ac10e1cef787ef5ecd5
김수진 <ksj030120@gmail.com>
2022-09-25 22:03:14 +0900

    cr + error_message + image

--- one_app/admin.py
+++ one_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content', 'created_at', 'updated_at',]
+    list_display_links = ['id', 'title']
---
+++ one_app/forms.py
@@ -0,0 +1,13 @@
+from dataclasses import fields
+from socket import fromshare
+from django import forms
+from .models import Post
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title','content','image',]
+
+    def __init__(self, *args, **kwargs):
+        super(PostForm, self).__init__(*args, **kwargs)
+        self.fields['title'].required = False
