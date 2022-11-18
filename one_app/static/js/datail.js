b4884778af69b1d13b8d24e04790432a4837d4c5
na hyun <lkg662130@naver.com>
2022-11-18 12:10:05 +0900

    merge하기 전 nana

--- one_app/static/js/datail.js
+++ one_app/static/js/datail.js
@@ -11,21 +11,3 @@ animationHeart.addEventListener('click', () => {
     animationHeart.classList.remove('animation');
     heart.classList.remove('fill-color');
 });
-
-// 모달 설정
-let modal = document.querySelector('#modal-notice'),
-    modalActive = document.querySelector('#modal-active'),
-    modalClose = document.querySelector('#modal-close');
-
-function activeModal() {
-    modal.classList.add('active');
-    document.querySelector('body').style.overflow = 'hidden';
-}
-
-function hideModal() {
-    modal.classList.remove('active');
-    document.querySelector('body').style.overflow = 'visible';
-}
-
-modalActive.addEventListener('click', activeModal)
-modalClose.addEventListener('click', hideModal)
